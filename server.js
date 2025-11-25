f72c4ba792d7fe748d841137c4e8e31b6bc1692f
akthernahela <nahela00@gmail.com>
2025-11-25 12:51:00 +0100

    Get all the element

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const PORT = 3000
 
 app.use(express.static('public'));
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
-})
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 
 app.get('/', (req, res) => {
-    res.send(`Server del mio blog`)
-})
+    res.send(`Server del mio blog`);
+});
+
+app.get('/posts', (req, res) => {
+    res.send(`Lista dei prodotti`);
+});
